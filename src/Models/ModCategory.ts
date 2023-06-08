7474225521e219b2c9911e7ba0fc08627cd7ce54
Kyler Workman <kworkman@gpsfx.com>
2023-06-08 11:08:57 -0600

    folder name supports 'fighter'

--- src/Models/ModCategory.ts
+++ src/Models/ModCategory.ts
@@ -1,10 +1,13 @@
-export type ModCategory = 'stage'|'ui'|'char'|'effect'|'param'|'audio'|'misc'
-export const NameToCategoryMapping:{[name in ModCategory]:string} = {
+export type ModCategory = 'stage'|'ui'|'char'|'fighter'|'effect'|'param'|'audio'|'music'|'misc'
+export type ArcropolisModCategory = 'Fighter'|'Stage'|'Effects'|'UI'|'Param'|'Audio'|'Misc'
+export const NameToCategoryMapping:{[name in ModCategory]:ArcropolisModCategory} = {
     char:'Fighter',
+    fighter:'Fighter',
     stage:'Stage',
     effect:'Effects',
     ui:'UI',
     param:'Param',
     audio:'Audio',
+    music:'Audio',
     misc:'Misc',
 }
