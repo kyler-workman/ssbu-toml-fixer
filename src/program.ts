624c8ab416073c92b205f309aa50400fc0faf827
Kyler Workman <kworkman@gpsfx.com>
2023-06-08 10:39:03 -0600

    Updated existing toml files

--- src/program.ts
+++ src/program.ts
@@ -14,20 +14,63 @@ export function runScript():void{
 }
 
 function processModFolder(runningDirectory:string, modFolderName:string){
-    var modInfo = getModInfo(runningDirectory, modFolderName);
+    var modInfo = getModInfo(modFolderName);
     var tomlFilePath = `${runningDirectory}/${modFolderName}/info.toml`
 
     if(!modInfo){
-        logFileAction('ERROR', modFolderName, "Invalid folder name, skipping...")
+        logFileAction('ERROR', modFolderName, "Invalid folder name, skipping")
     }else if(fs.existsSync(tomlFilePath)){
-        logFileAction('EDIT', modFolderName,'Existing file found, updating fields')
-        logFileAction('NONE', modFolderName,'Existing file found, matches format')
+        var properCategoryName = NameToCategoryMapping[modInfo.category];
+        var properDisplaynameLine = `display_name = "${modFolderName}"`;
+        var properCategoryLine = `category = "${properCategoryName}"`;
+
+        var tomlFile = fs.readFileSync(tomlFilePath).toString().split('\n');
+        var tomlFileIsEdited = false;
+
+        var displaynameLine = tomlFile.findIndex(v => /^display_name\s?=\s?/.test(v))
+        if(displaynameLine != -1){
+            var displayNameSearch = /"(.*?)"/.exec(tomlFile[displaynameLine]);
+            if(displayNameSearch && displayNameSearch[1]){
+                var displayNameValue = displayNameSearch[1];
+                if(displayNameValue != modFolderName){
+                    tomlFileIsEdited = true;
+                    tomlFile[displaynameLine] = properDisplaynameLine
+                }
+            }
+        }else{
+            tomlFileIsEdited = true;
+            tomlFile.push(properDisplaynameLine);
+        }
+
+        var categoryLine = tomlFile.findIndex(v => /^category\s?=\s?/.test(v))
+        if(categoryLine != -1){
+            var categorySearch = /"(.*?)"/.exec(tomlFile[categoryLine])
+            if(categorySearch && categorySearch[1]){
+                var existingCategoryValue = categorySearch[1];
+                if(existingCategoryValue != properCategoryName){
+                    tomlFileIsEdited = true;
+                    tomlFile[categoryLine] = properCategoryLine
+                }
+            }
+        }else{
+            tomlFileIsEdited = true;
+            tomlFile.push(properCategoryLine)
+        }
+
+        if(tomlFileIsEdited){
+            logFileAction('EDIT', modFolderName,'Existing file found, updating fields')
+            var newFile = tomlFile.join('\n');
+            fs.writeFileSync(tomlFilePath, newFile);
+        }else{
+            logFileAction('NONE', modFolderName,'Existing file found, matches format')
+        }
+
     }else{
-        logFileAction('NEW', modFolderName, "Missing .toml file, creating...")
+        logFileAction('NEW', modFolderName, "Missing .toml file, creating")
     }
 }
 
-function getModInfo(runningDirectory:string, modFolderName:string):ModInfo|null{
+function getModInfo(modFolderName:string):ModInfo|null{
     var info:ModInfo|null = null;
 
     var splitName = modFolderName.split('-');
@@ -47,5 +90,5 @@ function getModInfo(runningDirectory:string, modFolderName:string):ModInfo|null{
 type FileAction = 'NEW'|'EDIT'|'NONE'|'ERROR'
 var fileActionNameMaxLength = 5
 function logFileAction(action:FileAction, modFolderName:string, message:any){
-    console.log(`[ ${action.padEnd(4, ' ')} ] ${modFolderName} | ${message}`)
+    console.log(`[ ${action.padEnd(fileActionNameMaxLength, ' ')} ] ${modFolderName} | ${message}`)
 }
